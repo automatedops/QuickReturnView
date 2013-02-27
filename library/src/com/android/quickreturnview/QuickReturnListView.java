8ad2521a944a12ba96fb00aa893e0423eb6da870
WEI WU <wei@teambanjo.com>
2013-02-26 23:07:14 -0800

    init

---
+++ library/src/com/android/quickreturnview/QuickReturnListView.java
@@ -0,0 +1,19 @@
+package com.android.quickreturnview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+public class QuickReturnListView extends ListView {
+    public QuickReturnListView(Context context) {
+        super(context);
+    }
+
+    public QuickReturnListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public QuickReturnListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
